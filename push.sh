c6cb00255c56dfa150e1caa01c7e62a940fe3c08
ChoiIseungil <silly5921@kaist.ac.kr>
2021-11-21 07:56:04 +0000

    submodule push issues solved:

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 
 git add -u
 #큰 파일은 nas2에 넣기
-git commit -m "json generator started"
+git commit -m "pushed from EGG kairi_nvidia, by seungil"
 
 # git push https://ChoiIseungil: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@github.com/ChoiIseungil/vilbert-multi-task.git -u origin master --recurse-submodules=on-demand
 git push -u origin master --recurse-submodules=on-demand
